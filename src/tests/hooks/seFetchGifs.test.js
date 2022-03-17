cc9fa3ced101cc82f8ecf9ab8257b0af52689a69
KingAlex1997 <alex97l@hotmail.com>
2022-03-17 15:10:29 -0300

    prueba de cambio de estado en el custom hook

--- src/tests/hooks/seFetchGifs.test.js
+++ src/tests/hooks/seFetchGifs.test.js
@@ -2,10 +2,27 @@ import { renderHook } from "@testing-library/react-hooks";
 import { useFetchGifs } from "../../hooks/useFetchGifs";
 
 describe("Testeando el custom hook <seFetchGifs/>", () => {
-  test("Debe de retornar el estado inicial", () => {
-    const { result } = renderHook(() => useFetchGifs("One Punch"));
+  test("Debe de retornar el estado inicial", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGifs("One Punch")
+    );
     const { data, loading } = result.current;
+
+    await waitForNextUpdate();
+
     expect(data).toEqual([]);
     expect(loading).toBe(true);
+  });
+  test("debe de retornar un arreglo de imgs y loading sea false", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGifs("One Punch")
+    );
+
+    await waitForNextUpdate();
+
+    const { data, loading } = result.current;
+
+    expect(data.length).toEqual(10);
+    expect(loading).toBe(false);
   });
 });
